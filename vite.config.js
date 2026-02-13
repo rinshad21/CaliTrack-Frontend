1b17e33fcec87cf7a70454f68d2b158623689fbe
rinshad21 <mdrinshadka@gmail.com>
2026-02-13 20:31:20 +0530

    web app is now support pwa

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { analyzer } from "vite-bundle-analyzer";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,6 +14,35 @@ export default defineConfig({
       },
     }),
     analyzer({ analyzerMode: "static", open: false }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "CaliTrack",
+        short_name: "CaliTrack",
+        description: "Your trusted home workout buddy",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,woff2}"],
+      },
+    }),
   ],
   resolve: {
     alias: {
